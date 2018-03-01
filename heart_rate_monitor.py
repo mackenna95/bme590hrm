92b5b0c7a7f35888d03df09e3e893d2932d9abe8
mackenna95 <hillmj@umich.edu>
2018-03-01 15:28:47 -0500

    voltage_extreemes returns max and min from read in .csv file and passes pytest

--- heart_rate_monitor.py
+++ heart_rate_monitor.py
@@ -1,3 +1,4 @@
+import numpy as np
 from read_csv import ReadCsv
 
 
@@ -20,19 +21,34 @@ class HeartRateMonitor:
         interval (???): time interval for average heart rate
     """
 
-    def __init__(self, data, scale, interval):
+    def __init__(self, data_csv, scale, interval):
         self.mean_hr_bpm = None
         self.voltage_extreemes = None
         self.duration = None
         self.num_beats = None
         self.beats = None
-        self.return_mean_hr_bpm(data, scale, interval)
-        self.return_voltage_extreemes(data, scale)
-        self.return_duration(data, scale)
-        self.return_num_beats(data, scale)
-        self.return_beats(data, scale)
+        data = self.ReadCsv(data_csv, scale)
+        self.return_mean_hr_bpm(data, interval)
+        self.return_voltage_extreemes(data)
+        self.return_duration(data)
+        self.return_num_beats(data)
+        self.return_beats(data)
 
-    def return_mean_hr_bpm(self, data, scale, interval):
+    def ReadCsv(self, data_csv, scale):
+        """
+        :param self:    ECG data
+        :returns data:  ECG data as np array
+        """
+
+        import logging
+        logging.basicConfig(filename="heart_rate_monitor_log.txt",
+                            format='%(asctime)s %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S %p')
+        data_csv = ReadCsv(data_csv)
+        logging.info("Success: data as np array returned.")
+        return data_csv.data
+
+    def return_mean_hr_bpm(self, data, interval):
         """
         :param self:            ECG data
         :returns mean_hr_bpm:   heart rate over a user-specified minutes
@@ -64,7 +80,7 @@ class HeartRateMonitor:
         self.mean_hr_bpm = mean_hr_bpm
         logging.info("Success: mean_hr_bpm returned.")
 
-    def return_voltage_extreemes(self, data, scale):
+    def return_voltage_extreemes(self, data):
         """
         :param self:                ECG data
         :returns voltage_extreemes: tuple containing min, max lead voltages
@@ -83,7 +99,9 @@ class HeartRateMonitor:
             raise ValueError("list is empty")
 
         try:
-            voltage_extreemes = 1  # "CODE HERE" /////
+             tmax, vmax = np.max(data, axis=0)
+             tmin, vmin = np.min(data, axis=0)
+             voltage_extreemes = (vmax, vmin);
         except TypeError:
             logging.debug('TypeError: non-numeric')
             raise TypeError("List contains non-numeric elements.")
@@ -96,7 +114,7 @@ class HeartRateMonitor:
         self.voltage_extreemes = voltage_extreemes
         logging.info("Success: voltage_extreemes returned.")
 
-    def return_duration(self, data, scale):
+    def return_duration(self, data):
         """
         :param self:            ECG data
         :returns duration:      time duration of the ECG strip
@@ -128,7 +146,7 @@ class HeartRateMonitor:
         self.duration = duration  # SELF.BLANK HERE ////
         logging.info("Success: duration returned.")
 
-    def return_num_beats(self, data, scale):
+    def return_num_beats(self, data):
         """
         :param self:         ECG data
         :returns num_beats:  number of detected beats in the strip
@@ -160,7 +178,7 @@ class HeartRateMonitor:
         self.num_beats = num_beats  # SELF.BLANK HERE ////
         logging.info("Success: num_beats returned.")
 
-    def return_beats(self, data, scale):
+    def return_beats(self, data):
         """
         :param self:        ECG data
         :returns beats:     numpy array of times when a beat occurred
--- test_heart_rate_monitor.py
+++ test_heart_rate_monitor.py
@@ -2,23 +2,28 @@ def test_heart_rate_monitor():
     import pytest
     from heart_rate_monitor import HeartRateMonitor
 
+    tolerance = 0.0000001
+
     output = HeartRateMonitor("test_data1.csv", 1, 1)
     assert output.mean_hr_bpm == 1
-    assert output.voltage_extreemes == 1
+    assert output.voltage_extreemes[0] - 1.05 < tolerance
+    assert output.voltage_extreemes[1] + 0.68 < tolerance
     assert output.duration == 1
     assert output.num_beats == 1
     assert output.beats == 1
 
     output = HeartRateMonitor("test_data10.csv", 1, 1)
     assert output.mean_hr_bpm == 1
-    assert output.voltage_extreemes == 1
+    assert output.voltage_extreemes[0] - 1.555 < tolerance
+    assert output.voltage_extreemes[1] + 1.58 < tolerance
     assert output.duration == 1
     assert output.num_beats == 1
     assert output.beats == 1
 
     output = HeartRateMonitor("test_data15.csv", 1, 1)
     assert output.mean_hr_bpm == 1
-    assert output.voltage_extreemes == 1
+    assert output.voltage_extreemes[0] - 0.7 < tolerance
+    assert output.voltage_extreemes[1] + 0.33077 < tolerance
     assert output.duration == 1
     assert output.num_beats == 1
     assert output.beats == 1
